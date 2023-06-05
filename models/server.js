277c0c971e323d071212d041435e3cedc4627950
ET-Guerrero <133876450+ET-Guerrero@users.noreply.github.com>
2023-06-05 11:50:01 -0300

    Update server.js

--- models/server.js
+++ models/server.js
@@ -10,7 +10,7 @@ class Server {
 
     constructor(){
         this.app = express()
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios'
 
         //Middlewares: funcion3s que añaden otras funcionalidades al webserver
